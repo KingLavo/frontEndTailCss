e3ea7f75c68702ea47be967249816f42419f27c4
kingsley <uduma.kingsley74@gmail.com>
2024-05-21 15:51:19 +0100

    online blog working fine

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.(html, js)', 
+    "./public/**/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp')
+  ],
+}
+
